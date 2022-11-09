f180316c4513611090b5a377772fbc06a191ea67
Renaud Lespérance <renaudlesperance@outlook.com>
2022-11-09 09:07:46 -0500

    add dynamic interval to make graph look real time

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -2,7 +2,7 @@ import {useNavigate,useParams} from 'react-router-dom'
 import {Card, Row, Col, ButtonGroup, Button } from 'react-bootstrap'
 import Data from "../../data/data.json";
 
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -18,7 +18,6 @@ import styles from './Dashboard.module.css';
 import BackButton from '../backButton/BackButton';
 import ClickableContainer from '../clickableContainer/ClickableContainer';
 import CustomContainer from '../customContainer/CustomContainer';
-
 var testcount = 1
 
 ChartJS.register(
@@ -31,8 +30,17 @@ ChartJS.register(
   Legend
 );
 
+// const IntervalExample = () => {
+//   const [customParams, setcustomParams] = useState({min:0,d_max:3});
 
-const DrawLine = ({data_v,title,color,x_label,y_label}) => {
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       setcustomParams ({min:customParams.min+1,max:customParams.max+1})
+//     }, 1000);
+//   }, []);
+// };
+
+const DrawLine = ({data_v,d_min,d_max,title,color,x_label,y_label}) => {
   const labels = Data.labels;
 
   const data = {
@@ -41,8 +49,6 @@ const DrawLine = ({data_v,title,color,x_label,y_label}) => {
       {
         label: 'Dataset 1',
         data: data_v,
-        // borderColor: 'rgb(255, 99, 132)',
-        // backgroundColor: 'rgba(255, 99, 132, 0.5)',
         borderColor: color,
         backgroundColor: color,
       },
@@ -64,8 +70,8 @@ const DrawLine = ({data_v,title,color,x_label,y_label}) => {
     scales: {
       x: {
           type: 'linear',
-          min: 2,
-          max: 9,      
+          min: d_min,
+          max: d_max,      
           title: {
             display: true,
             text: x_label
@@ -97,6 +103,19 @@ const dataH = Data.hum_values;
 
 function StatCard () {
   const [active, setActive] = useState(1)
+  const [customParams, setCustomParams] = useState({min:0,max:3});
+
+  useEffect(() => {
+    var min = 0
+    var max = 10
+    const interval = setInterval(() => {
+      setCustomParams ({min,max})
+      min += 2
+      max += 2
+      console.log({min,max})
+    }, 2000);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <Card>
@@ -113,13 +132,13 @@ function StatCard () {
       <Card.Body>
         <Row>
           <Col>
-            <DrawLine data_v = {dataT} title = "Temperature" x_label = "Time []" y_label = "Temperature [C]" color="#ef4423"/>
+            <DrawLine data_v = {dataT} d_min = {customParams.min} d_max = {customParams.max} title = "Temperature" x_label = "Time []" y_label = "Temperature [C]" color="#ef4423"/>
           </Col>
           <Col>
-            <DrawLine data_v = {dataH} title = "Humidité" x_label = "Time []" y_label = "Humidité [g.m-3]" color="#628b3c"/>
+            <DrawLine data_v = {dataH} d_min = {customParams.min} d_max = {customParams.max} title = "Humidité" x_label = "Time []" y_label = "Humidité [g.m-3]" color="#628b3c"/>
           </Col>
           <Col>
-            <DrawLine data_v = {dataW} title = "Consomation d'Eau" x_label = "Time []" y_label = "Eau [L]" color="#010585"/>
+            <DrawLine data_v = {dataW} d_min = {customParams.min} d_max = {customParams.max} title = "Consomation d'Eau" x_label = "Time []" y_label = "Eau [L]" color="#010585"/>
           </Col>
         </Row>
       </Card.Body>
