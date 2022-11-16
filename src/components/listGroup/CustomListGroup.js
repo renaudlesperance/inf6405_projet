d08b05a90c3165bd9d1eb75309226b27738a1603
Jason Sautieres <jason.sautieres@gmail.com>
2022-11-16 18:17:19 -0500

    Finalize Sensor view

--- src/components/listGroup/CustomListGroup.js
+++ src/components/listGroup/CustomListGroup.js
@@ -2,11 +2,17 @@ import React, { useEffect, useState } from 'react';
 import styles from './CustomListGroup.module.css';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Form from 'react-bootstrap/Form';
-import { faSquarePlus } from '@fortawesome/free-solid-svg-icons'
+import { faSquarePlus, faSun, faDroplet, faTemperature4, faCloud } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import PropTypes from 'prop-types';
 
-
-export default function CustomListGroup({ data, onAdd, onClick, selectedItem, title }) {
+export default function CustomListGroup({ data, onAdd, onClick, selectedItem, title, withIcons }) {
+  const icons = {
+    "Luminosité": faSun,
+    "Humidité": faTemperature4,
+    "pH": faDroplet,
+    "CO2": faCloud
+  }
   const [entries, setEntries] = useState(data);
 
   useEffect(() => setEntries(data), [data])
@@ -17,7 +23,7 @@ export default function CustomListGroup({ data, onAdd, onClick, selectedItem, ti
   }
 
   return (
-    <ListGroup className={styles.listContainer} >
+    <ListGroup className={styles.listContainer} id='listContainer' >
       <ListGroup.Item className={styles.title}>
         {title}
         <FontAwesomeIcon icon={faSquarePlus} className={styles.icon} onClick={onAdd} />
@@ -27,7 +33,14 @@ export default function CustomListGroup({ data, onAdd, onClick, selectedItem, ti
       </ListGroup.Item>
       {entries.map((elt) => (
         <ListGroup.Item action variant="light" active={JSON.stringify(elt) === JSON.stringify(selectedItem)} key={Math.random()} onClick={() => onClick(elt)}>
-          {elt.name}
+          {withIcons ? (
+            <div className={styles.listWithIcons}>
+              <span>{elt.name}</span>
+              <FontAwesomeIcon icon={icons[elt?.type]} />
+            </div>
+          ) : (
+            elt.name
+          )}
         </ListGroup.Item>
       ))}
       {entries.length === 0 && (
@@ -37,4 +50,12 @@ export default function CustomListGroup({ data, onAdd, onClick, selectedItem, ti
       )}
     </ListGroup>
   );
+}
+
+CustomListGroup.defaultProps = {
+  withIcons: false,
+};
+
+CustomListGroup.propTypes = {
+  withIcons: PropTypes.bool,
 }
