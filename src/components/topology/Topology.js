7de73260012dc3da6d10f48eb0f6721452084fb1
Jason Sautieres <jason.sautieres@gmail.com>
2022-11-18 19:27:17 -0500

    fix topology size

--- src/components/topology/Topology.js
+++ src/components/topology/Topology.js
@@ -21,25 +21,25 @@ function Topology() {
       <CustomContainer>
         <div className={styles.title}><h1>Topologie de la serre n°{id}</h1></div>
         <Row>
-          <Col sm={9} className={styles.topoContainer}>
-          <Card>
-      <Card.Header as="h5">
-        <div className={styles.headerCardDashboard}>
-          <ToggleButtonGroup type="checkbox"  value={activeTopo} onChange={handleChange}>
-            <ToggleButton id="tbg-btn-1" variant="outline-secondary" value={1} disabled> Irrigation </ToggleButton>
-            <ToggleButton id="tbg-btn-2" variant="outline-secondary" value={2}> Capteurs </ToggleButton>
-            <ToggleButton id="tbg-btn-3" variant="outline-secondary" value={3}> Caméras </ToggleButton>
-          </ToggleButtonGroup>
-        </div>
-      </Card.Header>
-      <Card.Body>
-          {!activeTopo.includes(2) && !activeTopo.includes(3) &&(<img src={"https://drive.google.com/uc?export=view&id="+urlBaseTopo} className={styles.photo95}/>)}
-          {activeTopo.includes(2) && !activeTopo.includes(3) &&(<img src={"https://drive.google.com/uc?export=view&id="+urlTopoCapteur} className={styles.photo95}/>)}
-          {!activeTopo.includes(2) && activeTopo.includes(3) &&(<img src={"https://drive.google.com/uc?export=view&id="+urlTopoCameras} className={styles.photo95}/>)}
-          {activeTopo.includes(2) && activeTopo.includes(3) &&(<img src={"https://drive.google.com/uc?export=view&id="+urlTopoCapteurCameras} className={styles.photo95}/>)}
-      </Card.Body>
-    
-    </Card>
+          <Col className={styles.topoContainer}>
+            <Card>
+              <Card.Header as="h5">
+                <div className={styles.headerCardDashboard}>
+                  <ToggleButtonGroup type="checkbox"  value={activeTopo} onChange={handleChange}>
+                    <ToggleButton id="tbg-btn-1" variant="outline-secondary" value={1} disabled> Irrigation </ToggleButton>
+                    <ToggleButton id="tbg-btn-2" variant="outline-secondary" value={2}> Capteurs </ToggleButton>
+                    <ToggleButton id="tbg-btn-3" variant="outline-secondary" value={3}> Caméras </ToggleButton>
+                  </ToggleButtonGroup>
+                </div>
+              </Card.Header>
+              <Card.Body className={styles.align}>
+                  {!activeTopo.includes(2) && !activeTopo.includes(3) &&(<img src={"https://drive.google.com/uc?export=view&id="+urlBaseTopo} className={styles.photo95}/>)}
+                  {activeTopo.includes(2) && !activeTopo.includes(3) &&(<img src={"https://drive.google.com/uc?export=view&id="+urlTopoCapteur} className={styles.photo95}/>)}
+                  {!activeTopo.includes(2) && activeTopo.includes(3) &&(<img src={"https://drive.google.com/uc?export=view&id="+urlTopoCameras} className={styles.photo95}/>)}
+                  {activeTopo.includes(2) && activeTopo.includes(3) &&(<img src={"https://drive.google.com/uc?export=view&id="+urlTopoCapteurCameras} className={styles.photo95}/>)}
+              </Card.Body>
+
+            </Card>
           </Col>
         </Row>
       </CustomContainer>
