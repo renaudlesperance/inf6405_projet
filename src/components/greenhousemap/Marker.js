a0947b24dedd1384f5ec935ce96018bca1e9e886
Jason Sautieres <jason.sautieres@gmail.com>
2022-11-22 17:43:11 -0500

    Updates

--- src/components/greenhousemap/Marker.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from './Marker.module.css';
-
-export default function Marker({ onClick }) {
-
-  return (
-    <div className={styles.pin} onClick={onClick} />
-  );
-}
